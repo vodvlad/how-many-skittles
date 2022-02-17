84709e9a59d8ecf447cb1481c3655849973ea4b0
Vladyslav Vodopianov <vodvlad2001@gmail.com>
2022-02-17 18:35:11 +0100

    Update skittles.py

--- skittles.py
+++ skittles.py
@@ -15,7 +15,18 @@ def viewImage(image):
 
 
 def main(argv):
+    print(argv[2])
+    thisdict = {
+        "red": [[173, 0, 115], [179, 255, 255]],
+        "green": [[34, 0, 0], [83, 255, 206]],
+        "blue": [[94, 27, 17], [132, 255, 255]]
+    }
+    temp = thisdict[argv[2]]
+    o_low = np.array(temp[0])
+    o_high = np.array(temp[1])
+
     list = []
+
     for filename in os.listdir(argv[1]):
         if filename[filename.rfind(".") + 1:] in ['jpg', 'jpeg', 'png']:
                 list.append(filename)
@@ -29,8 +40,9 @@ def main(argv):
         image_e = exposure.adjust_gamma(image, 2)
         hsv_img = cv2.cvtColor(image_e, cv2.COLOR_BGR2HSV)
 
-        o_low = np.array([94, 27, 17])
-        o_high = np.array([132, 255, 255])
+        
+        
+
         mask = cv2.inRange(hsv_img, o_low, o_high)
         viewImage(mask)
 
@@ -40,7 +52,7 @@ def main(argv):
         labels = label(cl)
         plt.imshow(labels)
         plt.show()
-        print(labels.max())
+        print(full_name + ': ' + str(labels.max()))
 
 if __name__ == "__main__":
     main(sys.argv)
